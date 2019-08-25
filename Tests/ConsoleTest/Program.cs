2277768355530ea5baffacbb3deb6503d00384c3
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-08-25 13:20:03 +0300

    Тестовый проект + импорт инструментов для XML

---
+++ Tests/ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathCore;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var z = new Complex(5, 7);
+            Console.WriteLine(z);
+
+            Console.ReadLine();
+        }
+    }
+}
