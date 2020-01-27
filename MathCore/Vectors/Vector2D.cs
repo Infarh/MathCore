b94ac1ce7bd820df11a24527604e70b29ca26a82
Infarh <shmachilin@yandex.ru>
2020-01-28 00:02:09 +0300

    Vector2D - комментарии

--- MathCore/Vectors/Vector2D.cs
+++ MathCore/Vectors/Vector2D.cs
@@ -115,25 +115,52 @@ namespace MathCore.Vectors
         /// <returns>Вектор, координаты которого имеют противоположенный знак</returns>
         public static Vector2D operator -(in Vector2D a) => new Vector2D(-a._X, -a._Y);
 
+        /// <summary>Оператор вычисления суммы двух векторов</summary>
+        /// <returns>Вектор, координаты которого составлены из суммы координат слагаемых</returns>
         public static Vector2D operator +(in Vector2D a, in Vector2D b) => new Vector2D(a._X + b._X, a._Y + b._Y);
 
+        /// <summary>Оператор вычисления разности двух векторов</summary>
+        /// <returns>Вектор, координаты которого составлены из разности координат слагаемых</returns>
         public static Vector2D operator -(in Vector2D a, in Vector2D b) => new Vector2D(a._X - b._X, a._Y - b._Y);
 
+        /// <summary>Оператор вычисления скалярного произведения двух векторов</summary>
+        /// <returns>Вещественное число, равное суме попарных произведений координат векторов</returns>
         public static double operator *(in Vector2D a, in Vector2D b) => a._X * b._X + a._Y * b._Y;
 
+        /// <summary>Оператор вычисления суммы вектора и вещественного числа</summary>
+        /// <returns>Вектор, координаты которого увеличены на значение скалярного слагаемого</returns>
         public static Vector2D operator +(in Vector2D a, double b) => new Vector2D(a._X + b, a._Y + b);
 
+        /// <summary>Оператор вычисления суммы вектора и вещественного числа</summary>
+        /// <returns>Вектор, координаты которого увеличены на значение скалярного слагаемого</returns>
+        public static Vector2D operator +(double a, in Vector2D b) => new Vector2D(a + b._X, a + b._Y);
+
+        /// <summary>Оператор вычитания скалярного значения из вектора</summary>
+        /// <param name="a">Уменьшаемое - вектор</param>
+        /// <param name="b">Вычитаемое - скаляр</param>
+        /// <returns>Вектор, координаты которого уменьшены на значение скаляра</returns>
         public static Vector2D operator -(in Vector2D a, double b) => new Vector2D(a._X - b, a._Y - b);
 
-        public static Vector2D operator *(Vector2D a, double b) => new Vector2D(a._X * b, a._Y * b);
-
-        public static Vector2D operator /(in Vector2D a, double b) => new Vector2D(a._X / b, a._Y / b);
-
-        public static Vector2D operator +(double a, in Vector2D b) => new Vector2D(a + b._X, a + b._Y);
+        /// <summary>Оператор вычитания вектора из скалярного значения</summary>
+        /// <param name="b">Уменьшаемое - скаляр</param>
+        /// <param name="a">вычитаемое - вектор</param>
+        /// <returns>Вектор, координаты которого вычтены из скалярного значения</returns>
         public static Vector2D operator -(double a, in Vector2D b) => new Vector2D(a - b._X, a - b._Y);
 
+        /// <summary>Оператор умножения вектора на число</summary>
+        /// <returns>Вектор, координаты которого умножены на скалярное значение</returns>
+        public static Vector2D operator *(in Vector2D a, double b) => new Vector2D(a._X * b, a._Y * b);
+
+        /// <summary>Оператор умножения вектора на число</summary>
+        /// <returns>Вектор, координаты которого умножены на скалярное значение</returns>
         public static Vector2D operator *(double a, in Vector2D b) => new Vector2D(a * b._X, a * b._Y);
 
+        /// <summary>Оператор деления вектора на число</summary>
+        /// <returns>Вектор, координаты которого разделены на скалярное значение</returns>
+        public static Vector2D operator /(in Vector2D a, double b) => new Vector2D(a._X / b, a._Y / b);
+
+        /// <summary>Оператор деления числа на вектор</summary>
+        /// <returns>Вектор, координаты которого образованы делением координат числа на координаты исходного вектора</returns>
         public static Vector2D operator /(double a, in Vector2D b) => new Vector2D(a / b._X, a / b._Y);
 
         /// <summary>Оператор проекции в вектора на вектор</summary>
@@ -142,43 +169,88 @@ namespace MathCore.Vectors
         /// <returns>Проекция первого вектора на второй</returns>
         public static double operator ^(in Vector2D a, in Vector2D b) => a * b / (a.R * b.R);
 
+        /// <summary>Оператор проверки на параллельность двух векторов</summary>
+        /// <returns>Истина, если вектора параллельны</returns>
         public static bool operator |(in Vector2D a, in Vector2D b) => (a ^ b).Equals(1d);
 
+        /// <summary>Оператор проверки на перпендикулярность двух векторов</summary>
+        /// <returns>Истина, если вектора перпендикулярны</returns>
         public static bool operator &(in Vector2D a, in Vector2D b) => (a ^ b).Equals(0d);
 
+        /// <summary>Оператор вычисления проекции вектора на вектор</summary>
+        /// <param name="a">Проецируемый вектор</param>
+        /// <param name="b">Вектор, на который осуществляется проекция</param>
+        /// <returns>Длина проекции</returns>
         public static double operator %(in Vector2D a, in Vector2D b) => a * b / b.R;
 
+        /// <summary>Оператор представление вектора в базисе</summary>
+        /// <param name="a">Вектор, представляемый в базисе</param>
+        /// <param name="b">Базис, в котором надо представить вектор</param>
+        /// <returns>Вектор, представленный в базисе</returns>
         public static Vector2D operator %(in Vector2D a, in Basis2D b) => a.InBasis(b);
 
+        /// <summary>Оператор суммы вектора и вещественного числа одинарной точности</summary>
+        /// <returns>Вектор, координаты которого увеличены на значение числа одинарной точности</returns>
         public static Vector2D operator +(in Vector2D a, float b) => new Vector2D(a._X + b, a._Y + b);
 
+        /// <summary>Оператор разности вектора и вещественного числа одинарной точности</summary>
+        /// <returns>Вектор, координаты которого уменьшены на значение числа одинарной точности</returns>
         public static Vector2D operator -(in Vector2D a, float b) => new Vector2D(a._X - b, a._Y - b);
 
+        /// <summary>Оператор произведения вектора и вещественного числа одинарной точности</summary>
+        /// <returns>Вектор, координаты которого умножены на значение числа одинарной точности</returns>
         public static Vector2D operator *(in Vector2D a, float b) => new Vector2D(a._X * b, a._Y * b);
 
+        /// <summary>Оператор деления вектора и вещественного числа одинарной точности</summary>
+        /// <returns>Вектор, координаты которого поделены на значение числа одинарной точности</returns>
         public static Vector2D operator /(in Vector2D a, float b) => new Vector2D(a._X / b, a._Y / b);
 
+        /// <summary>Оператор суммы вектора и вещественного числа одинарной точности</summary>
+        /// <returns>Вектор, координаты которого увеличены на значение числа одинарной точности</returns>
         public static Vector2D operator +(float a, in Vector2D b) => new Vector2D(a + b._X, a + b._Y);
 
+        /// <summary>Оператор вещественного числа одинарной точности и разности вектора</summary>
+        /// <returns>Вектор, координаты которого равны разности числа одинарной точности и координат исходного вектора</returns>
         public static Vector2D operator -(float a, in Vector2D b) => new Vector2D(a - b._X, a - b._Y);
 
+        // <summary>Оператор произведения вектора и вещественного числа одинарной точности</summary>
+        /// <returns>Вектор, координаты которого умножены на значение числа одинарной точности</returns>
         public static Vector2D operator *(float a, in Vector2D b) => new Vector2D(a * b._X, a * b._Y);
 
+        /// <summary>Оператор деления вещественного числа одинарной точности и вектора</summary>
+        /// <returns>Вектор, координаты которого равны отношению числа одинарной точности и координат исходного вектора</returns>
         public static Vector2D operator /(float a, in Vector2D b) => new Vector2D(a / b._X, a / b._Y);
 
+        /// <summary>Оператор суммы вектора и вещественного целого числа</summary>
+        /// <returns>Вектор, координаты которого увеличены на значение целого числа</returns>
         public static Vector2D operator +(in Vector2D a, int b) => new Vector2D(a._X + b, a._Y + b);
 
+        /// <summary>Оператор разности вектора и вещественного целого числа</summary>
+        /// <returns>Вектор, координаты которого уменьшены на значение целого числа</returns>
         public static Vector2D operator -(in Vector2D a, int b) => new Vector2D(a._X - b, a._Y - b);
 
+        /// <summary>Оператор произведения вектора и целого числа</summary>
+        /// <returns>Вектор, координаты которого умножены на значение целого числа</returns>
         public static Vector2D operator *(in Vector2D a, int b) => new Vector2D(a._X * b, a._Y * b);
 
+        /// <summary>Оператор деления вектора и целого числа</summary>
+        /// <returns>Вектор, координаты которого поделены на значение целого числа</returns>
         public static Vector2D operator /(in Vector2D a, int b) => new Vector2D(a._X / b, a._Y / b);
 
+        /// <summary>Оператор произведения вектора и целого числа</summary>
+        /// <returns>Вектор, координаты которого умножены на значение целого числа</returns>
         public static Vector2D operator +(int a, in Vector2D b) => new Vector2D(a + b._X, a + b._Y);
+
+        /// <summary>Оператор целого числа и разности вектора</summary>
+        /// <returns>Вектор, координаты которого равны разности целого числа и координат исходного вектора</returns>
         public static Vector2D operator -(int a, in Vector2D b) => new Vector2D(a - b._X, a - b._Y);
 
+        /// <summary>Оператор произведения вектора и целого числа</summary>
+        /// <returns>Вектор, координаты которого умножены на значение целого числа</returns>
         public static Vector2D operator *(int a, in Vector2D b) => new Vector2D(a * b._X, a * b._Y);
 
+        /// <summary>Оператор деления целого числа и вектора</summary>
+        /// <returns>Вектор, координаты которого равны отношению целого числа и координат исходного вектора</returns>
         public static Vector2D operator /(int a, in Vector2D b) => new Vector2D(a / b._X, a / b._Y);
 
         #endregion
