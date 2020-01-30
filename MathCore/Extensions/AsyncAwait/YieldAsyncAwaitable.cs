7c701967c30bbc9ec64dc856795ff48cabce98c4
Infarh <shmachilin@yandex.ru>
2020-01-31 00:28:04 +0300

    Рефакторинг

--- MathCore/Extensions/AsyncAwait/YieldAsyncAwaitable.cs
+++ MathCore/Extensions/AsyncAwait/YieldAsyncAwaitable.cs
@@ -2,18 +2,26 @@
 using System.Runtime.InteropServices;
 using System.Security;
 using MathCore.Annotations;
+// ReSharper disable UnusedMember.Global
 
 // ReSharper disable once CheckNamespace
 namespace System.Threading.Tasks
 {
     [StructLayout(LayoutKind.Sequential, Size = 1)]
+#pragma warning disable CA1815 // Override equals and operator equals on value types
     public struct YieldAsyncAwaitable
+#pragma warning restore CA1815 // Override equals and operator equals on value types
     {
+#pragma warning disable CA1822 // Mark members as static
         public YieldAsyncAwaiter GetAwaiter() => new YieldAsyncAwaiter();
+#pragma warning restore CA1822 // Mark members as static
 
         [StructLayout(LayoutKind.Sequential, Size = 1)]
         //[HostProtection(SecurityAction.LinkDemand, ExternalThreading = true, Synchronization = true)]
+        // ReSharper disable once RedundantExtendsListEntry
+#pragma warning disable CA1815 // Override equals and operator equals on value types
         public struct YieldAsyncAwaiter : ICriticalNotifyCompletion, INotifyCompletion
+#pragma warning restore CA1815 // Override equals and operator equals on value types
         {
             [NotNull] private static readonly WaitCallback __WaitCallbackRunAction = RunAction;
             [NotNull] private static readonly SendOrPostCallback __SendOrPostCallbackRunAction = RunAction;
@@ -39,7 +47,9 @@ namespace System.Threading.Tasks
             [SecurityCritical]
             public void UnsafeOnCompleted([NotNull] Action continuation) => QueueContinuation(continuation, false);
 
+#pragma warning disable CA1822 // Mark members as static
             public void GetResult() { }
+#pragma warning restore CA1822 // Mark members as static
         }
     }
 }
