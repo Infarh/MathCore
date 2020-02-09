ebb85e190c76a2adb8b5842df0fbc811f09d586c
Infarh <shmachilin@yandex.ru>
2020-02-09 23:24:56 +0300

    Комментарии

--- MathCore/Extensions/BindingListExtensions.cs
+++ MathCore/Extensions/BindingListExtensions.cs
@@ -1,9 +1,17 @@
-﻿
-// ReSharper disable once CheckNamespace
+﻿// ReSharper disable once CheckNamespace
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
 namespace System.ComponentModel
 {
+    /// <summary>Класс методов-расширений для <see cref="BindingList{T}"/></summary>
     public static class BindingListExtensions
     {
+        /// <summary>Инициализация нового экземпляра <see cref="BindingList{T}"/> с помощью функции</summary>
+        /// <typeparam name="T">Тип элементов списка</typeparam>
+        /// <param name="list">Инициализируемый <see cref="BindingList{T}"/></param>
+        /// <param name="ElementsCount">Число добавляемых элементов</param>
+        /// <param name="Initializer">Функция генерации новых элементов списка</param>
+        /// <returns>Инициализируемый <see cref="BindingList{T}"/></returns>
         public static BindingList<T> Initialize<T>
         (
             this BindingList<T> list,
@@ -16,6 +24,14 @@ namespace System.ComponentModel
             return list;
         }
 
+        /// <summary>Инициализация нового экземпляра <see cref="BindingList{T}"/> с помощью функции</summary>
+        /// <typeparam name="T">Тип элементов списка</typeparam>
+        /// <typeparam name="TParameter">Тип параметра инициализации</typeparam>
+        /// <param name="list">Инициализируемый <see cref="BindingList{T}"/></param>
+        /// <param name="ElementsCount">Число добавляемых элементов</param>
+        /// <param name="parameter">Параметр процесса инициализации, передаваемый в функцию</param>
+        /// <param name="Initializer">Функция генерации новых элементов списка</param>
+        /// <returns>Инициализируемый <see cref="BindingList{T}"/></returns>
         public static BindingList<T> Initialize<T, TParameter>
         (
             this BindingList<T> list,
--- MathCore/Extensions/BoolExtensions.cs
+++ MathCore/Extensions/BoolExtensions.cs
@@ -1,13 +1,46 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using MathCore.Annotations;
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedType.Global
 
 // ReSharper disable once CheckNamespace
 namespace System
 {
+    /// <summary>Методы-расширения для логических значений</summary>
     public static class BoolExtensions
     {
+        /// <summary>
+        /// Вычислить поэлементную операцию И со всеми элементами перечисления.
+        /// Все элементы перечисления будут перебраны в обязательном порядке
+        /// </summary>
+        /// <param name="items">Перечисление логических значений</param>
+        /// <returns>Результат вычисления логического И для всех значений перечисления</returns>
         public static bool And([NotNull] this IEnumerable<bool> items) => items.Aggregate(true, (r, v) => r && v);
+
+        /// <summary>
+        /// Вычислить поэлементную операцию (ленивое) И со всеми элементами перечисления.
+        /// Перебор элементов перечисления производится до появления первого значения <see langword="false"/>
+        /// </summary>
+        /// <param name="items">Перечисление логических значений</param>
+        /// <returns>Результат вычисления логического И для значений перечисления</returns>
+        public static bool AndLazy([NotNull] this IEnumerable<bool> items) => items.Any(item => !item);
+
+
+        /// <summary>
+        /// Вычислить поэлементную операцию ИЛИ со всеми элементами перечисления.
+        /// Все элементы перечисления будут перебраны в обязательном порядке
+        /// </summary>
+        /// <param name="items">Перечисление логических значений</param>
+        /// <returns>Результат вычисления логического ИЛИ для всех значений перечисления</returns>
         public static bool Or([NotNull] this IEnumerable<bool> items) => items.Aggregate(false, (r, v) => r || v);
+
+        /// <summary>
+        /// Вычислить поэлементную операцию (ленивое) ИЛИ со всеми элементами перечисления.
+        /// Перебор элементов перечисления производится до появления первого значения <see langword="false"/>
+        /// </summary>
+        /// <param name="items">Перечисление логических значений</param>
+        /// <returns>Результат вычисления логического ИЛИ для значений перечисления</returns>
+        public static bool OrLazy([NotNull] this IEnumerable<bool> items) => items.Any(item => item);
     }
 }
