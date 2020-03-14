618c90b6d71670731a4a5f74338708343c3200a1
Infarh <shmachilin@yandex.ru>
2020-03-14 08:37:37 +0300

    Правка модульного теста Vector3DTests.InBasis_Test

--- Tests/MathCore.Tests/Vectors/Vector3DTests.cs
+++ Tests/MathCore.Tests/Vectors/Vector3DTests.cs
@@ -274,7 +274,8 @@ namespace MathCore.Tests.Vectors
         [TestMethod, Priority(4), Description("Тестирование метода преобразования вектора в базисе")]
         public void InBasis_Test()
         {
-            var x = Vector3D.Random();
+            var seed = (int)DateTime.Now.Ticks;
+            var x = Vector3D.Random(rnd: new Random(seed));
             var b = new Basis3D(1, 0, 0,
                                 0, 1, 0,
                                 0, 0, 1);
@@ -293,17 +294,19 @@ namespace MathCore.Tests.Vectors
             b = Basis3D.RotateOZ(angle);
             var y = x.InBasis(b);
 
-            Assert.AreEqual(angle, (y.AngleXOY - x.AngleXOY).GetAbsMod(2 * pi), eps,
-                "\r\n|(y.AngleXOY - x.AngleXOY) - angle| = {0:E}" +
-                "\r\nangle = {1}·π" +
-                "\r\nx.AngleXOY = {2}·π - {3}" +
-                "\r\nx.AngleXOY = {4}·π - {5}" +
-                "\r\ny.AngleXOY - x.AngleXOY = {6}·π",
+            Assert.AreEqual(angle, (y.AngleXOY - x.AngleXOY + 2 * pi).GetAbsMod(2 * pi), eps,
+                "\r\n\t|(y.AngleXOY - x.AngleXOY) - angle| = {0:E}" +
+                "\r\n\tangle = {1}·π" +
+                "\r\n\tx.AngleXOY = {2}·π - {3}" +
+                "\r\n\ty.AngleXOY = {4}·π - {5}" +
+                "\r\n\ty.AngleXOY - x.AngleXOY = {6}·π"+
+                "\r\n\tseed = {7}",
                 Abs(y.AngleXOY - x.AngleXOY - angle),
                 angle / Consts.pi,
                 x.AngleXOY / pi, x,
                 y.AngleXOY / pi, y,
-                (y.AngleXOY - x.AngleXOY) / pi);
+                (y.AngleXOY - x.AngleXOY) / pi,
+                seed);
         }
 
         /// <summary>Тестирование метода определения проекции вектора на вектор</summary>
