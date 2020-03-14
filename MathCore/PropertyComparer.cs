1db6047f8da14aae2eef8bbe836612e3aa347642
Infarh <shmachilin@yandex.ru>
2020-03-14 07:42:07 +0300

    Объекты сравнения вложенных значений объектов, позволяющие использовать лямбда-выражения для извлечения значений свйосвт для осуществления сравнения

---
+++ MathCore/PropertyComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MathCore.Annotations;
+
+namespace MathCore
+{
+    public class PropertyComparer<T, TValue> : IComparer<T>
+    {
+        [NotNull] private readonly Func<T, TValue> _Selector;
+
+        public PropertyComparer([NotNull] Func<T, TValue> Selector) => _Selector = Selector ?? throw new ArgumentNullException(nameof(Selector));
+
+        public int Compare(T x, T y) => Comparer<TValue>.Default.Compare(_Selector(x), _Selector(y));
+    }
+
+    public class PropertyComparer : IComparer
+    {
+        [NotNull] public static PropertyComparer<T, TValue> Create<T, TValue>([NotNull] Func<T, TValue> Selector) => new PropertyComparer<T, TValue>(Selector);
+
+        [NotNull] private readonly Func<object, object> _Selector;
+
+        public PropertyComparer([NotNull] Func<object, object> Selector) => _Selector = Selector ?? throw new ArgumentNullException(nameof(Selector));
+
+        public int Compare(object x, object y) => Comparer.Default.Compare(_Selector(x), _Selector(y));
+    }
+}
---
+++ MathCore/PropertyEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MathCore.Annotations;
+
+namespace MathCore
+{
+    public class PropertyEqualityComparer<T, TValue> : IEqualityComparer<T>
+    {
+        [NotNull] private readonly Func<T, TValue> _Selector;
+
+        public PropertyEqualityComparer([NotNull] Func<T, TValue> Selector) => _Selector = Selector ?? throw new ArgumentNullException(nameof(Selector));
+
+        public bool Equals(T x, T y) => EqualityComparer<TValue>.Default.Equals(_Selector(x), _Selector(y));
+
+        public int GetHashCode(T obj) => EqualityComparer<TValue>.Default.GetHashCode(_Selector(obj));
+    }
+
+    public class PropertyEqualityComparer : IEqualityComparer
+    {
+        [NotNull] public static PropertyEqualityComparer<T, TValue> Create<T, TValue>([NotNull] Func<T, TValue> Selector) => new PropertyEqualityComparer<T, TValue>(Selector);
+
+        [NotNull] private readonly Func<object, object> _Selector;
+
+        public PropertyEqualityComparer([NotNull] Func<object, object> Selector) => _Selector = Selector ?? throw new ArgumentNullException(nameof(Selector));
+
+        public new bool Equals(object x, object y) => object.Equals(_Selector(x), _Selector(y));
+
+        public int GetHashCode(object obj) => _Selector(obj).GetHashCode();
+    }
+}
