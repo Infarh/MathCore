b7aa4c2ec92844e8070a93f2c37c8dafd24c3b7b
Infarh <shmachilin@yandex.ru>
2020-03-26 19:43:06 +0300

    Доработка самого запроса к CSV

--- MathCore/CSV/CSVQuery.cs
+++ MathCore/CSV/CSVQuery.cs
@@ -17,10 +17,19 @@ namespace MathCore.CSV
         private readonly int _SkipRowsAfterHeader;
         private readonly char _ValuesSeparator;
         private readonly int _TakeRows;
+        private readonly IDictionary<string, int> _Header;
 
-        public CSVQuery([NotNull] FileInfo File) : this(File, 0, false, 0, ';', -1) { }
+        public CSVQuery([NotNull] FileInfo File) : this(File, 0, false, 0, ';', -1, null) { }
 
-        private CSVQuery(FileInfo File, int SkipRows, bool ContainsHeader, int SkipRowsAfterHeader, char ValuesSeparator, int TakeRows)
+        private CSVQuery(
+            [NotNull] FileInfo File,
+            int SkipRows,
+            bool ContainsHeader,
+            int SkipRowsAfterHeader,
+            char ValuesSeparator,
+            int TakeRows,
+            IDictionary<string, int> Header
+            )
         {
             _File = File ?? throw new ArgumentNullException(nameof(File));
             _SkipRows = SkipRows;
@@ -28,31 +37,38 @@ namespace MathCore.CSV
             _SkipRowsAfterHeader = SkipRowsAfterHeader;
             _ValuesSeparator = ValuesSeparator;
             _TakeRows = TakeRows;
+            _Header = Header;
         }
 
         public CSVQuery Skip(int RowsCount) => new CSVQuery(
-            _File, 
-            RowsCount, 
-            _ContainsHeader, 
+            _File,
+            RowsCount,
+            _ContainsHeader,
             _SkipRowsAfterHeader,
             _ValuesSeparator,
-            _TakeRows);
+            _TakeRows,
+            _Header
+            );
 
         public CSVQuery SkipAfterHeader(int RowsCount) => new CSVQuery(
-            _File, 
-            _SkipRows, 
-            _ContainsHeader, 
+            _File,
+            _SkipRows,
+            _ContainsHeader,
             RowsCount,
             _ValuesSeparator,
-            _TakeRows);
+            _TakeRows,
+            _Header
+        );
 
-        public CSVQuery ReadHeader(bool IsExist = true) => new CSVQuery(
-            _File, 
-            _SkipRows, 
+        public CSVQuery WithHeader(bool IsExist = true) => new CSVQuery(
+            _File,
+            _SkipRows,
             IsExist,
             _SkipRowsAfterHeader,
             _ValuesSeparator,
-            _TakeRows);
+            _TakeRows,
+            _Header
+        );
 
         public CSVQuery ValuesSeparator(char Separator) => new CSVQuery(
             _File,
@@ -60,7 +76,9 @@ namespace MathCore.CSV
             _ContainsHeader,
             _SkipRowsAfterHeader,
             Separator,
-            _TakeRows);
+            _TakeRows,
+            _Header
+        );
 
         public CSVQuery TakeRows(int RowsCount) => new CSVQuery(
             _File,
@@ -68,7 +86,68 @@ namespace MathCore.CSV
             _ContainsHeader,
             _SkipRowsAfterHeader,
             _ValuesSeparator,
-            RowsCount);
+            RowsCount,
+            _Header
+        );
+
+        public CSVQuery Header(IDictionary<string, int> Header) => new CSVQuery(
+            _File,
+            _SkipRows,
+            _ContainsHeader,
+            _SkipRowsAfterHeader,
+            _ValuesSeparator,
+            _TakeRows,
+            Header
+        );
+
+        [NotNull]
+        private static IDictionary<string, int> Merge([CanBeNull] IDictionary<string, int> Source, [CanBeNull] IDictionary<string, int> Values = null)
+        {
+            var result = Source is null || Source.Count == 0
+                ? new SortedList<string, int>()
+                : new SortedList<string, int>(Source);
+
+            if (Values is null || Values.Count == 0) return result;
+
+            foreach (var (key, value) in Values)
+                result[key] = value;
+
+            return result;
+        }
+
+        public CSVQuery MergeHeader(IDictionary<string, int> Header) => new CSVQuery(
+            _File,
+            _SkipRows,
+            _ContainsHeader,
+            _SkipRowsAfterHeader,
+            _ValuesSeparator,
+            _TakeRows,
+            Merge(_Header, Header)
+        );
+
+        [NotNull]
+        public IDictionary<string, int> GetHeader()
+        {
+            using var reader = _File.OpenText();
+            for (var i = 0; i < _SkipRows && !reader.EndOfStream; i++)
+                reader.ReadLine();
+
+            if (reader.EndOfStream)
+                throw new FormatException("Неожиданный конец файла");
+
+            char[] separator = { _ValuesSeparator };
+            var header_line = reader.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(header_line))
+                throw new FormatException("Пустая строка заголовка");
+
+            var headers = header_line.Split(separator);
+            var header = Merge(_Header);
+            for (var i = 0; i < headers.Length; i++)
+                header[headers[i]] = i;
+
+            return header;
+        }
 
         public IEnumerator<CSVQueryRow> GetEnumerator()
         {
@@ -77,15 +156,15 @@ namespace MathCore.CSV
             for (var i = 0; i < _SkipRows && !reader.EndOfStream; i++)
                 reader.ReadLine();
 
-            if(reader.EndOfStream) yield break;
+            if (reader.EndOfStream) yield break;
 
             char[] separator = { _ValuesSeparator };
 
-            SortedList<string, int> header = null;
+            var header = Merge(_Header);
             if (_ContainsHeader)
             {
                 var header_line = reader.ReadLine();
-                if(!string.IsNullOrWhiteSpace(header_line))
+                if (!string.IsNullOrWhiteSpace(header_line))
                 {
                     var headers = header_line.Split(separator);
                     header = new SortedList<string, int>();
@@ -96,7 +175,6 @@ namespace MathCore.CSV
 
             for (var i = 0; i < _SkipRowsAfterHeader && !reader.EndOfStream; i++)
                 reader.ReadLine();
-
 
             var index = 0;
             while (!reader.EndOfStream && index != _TakeRows)
--- MathCore/CSV/CSVQueryRow.cs
+++ MathCore/CSV/CSVQueryRow.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MathCore.Annotations;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace MathCore.CSV
 {
     public readonly struct CSVQueryRow
     {
         private readonly string[] _Items;
-        private readonly IReadOnlyDictionary<string, int> _Header;
+        private readonly IDictionary<string, int> _Header;
 
         public int ItemsCount => _Items.Length;
 
         public int Index { get; }
 
+        public IReadOnlyDictionary<string, int> Headers => new ReadOnlyDictionary<string, int>(_Header);
+
         public ref string this[int ValueIndex] => ref _Items[ValueIndex];
 
-        public ref string this[string ValueName] => ref _Items[_Header[ValueName]];
+        public ref string this[[NotNull] string ValueName] => ref _Items[_Header[ValueName]];
 
-        public CSVQueryRow(int Index, string[] Items, IReadOnlyDictionary<string, int> Header)
+        public CSVQueryRow(int Index, string[] Items, IDictionary<string, int> Header)
         {
             this.Index = Index;
             _Items = Items;
@@ -25,6 +30,6 @@ namespace MathCore.CSV
 
         public T ValueAs<T>(int ValueIndex) => (T)Convert.ChangeType(this[ValueIndex], typeof(T));
 
-        public T ValueAs<T>(string ValueName) => ValueAs<T>(_Header[ValueName]);
+        public T ValueAs<T>([NotNull] string ValueName) => ValueAs<T>(_Header[ValueName]);
     }
 }
