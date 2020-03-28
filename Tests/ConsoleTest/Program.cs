1209be5bfcb34fcdbd97f02280206aa2d98235b0
Infarh <shmachilin@yandex.ru>
2020-03-28 12:27:48 +0300

    Доработка интерфейса CSV

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
@@ -13,22 +15,54 @@ namespace ConsoleTest
         private static void Main()
         {
             const string base_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/";
-            const string confirmed = "time_series_covid19_confirmed_global.csv";
+            const string confirmed_file = "time_series_covid19_confirmed_global.csv";
 
-            static async Task<TextReader> GetReader(string address)
+            static async Task<TextReader> GetReaderAsync(string address)
             {
                 var client = new HttpClient { BaseAddress = new Uri(base_url) };
                 var response = await client.GetAsync(address);
                 return new StreamReader(await response.EnsureSuccessStatusCode().Content.ReadAsStreamAsync());
             }
 
-            var confirmed_query = new CSVQuery(() => GetReader(confirmed).Result)
-                .ValuesSeparator(',')
-                .WithHeader();
+            var confirmed = new CSVQuery(() => GetReaderAsync(confirmed_file).Result)
+                .WithHeader()
+                .AddColumn("Province", 0)
+                .AddColumn("Name", 1);
 
-            var header = confirmed_query.GetHeader();
+
+            var countries = confirmed.Select(country => (Country: country["Name"], Count: country.LastValue<int>()));
+
+            //foreach (var country in countries.OrderByDescending(c => c.Count))
+            //    Console.WriteLine(country);
+
+            var countries_v = confirmed.Select(country => (
+                    name: country["Name"].Trim('"', '*'),
+                    province: country["Province"],
+                    values: country
+                       .Skip(6)
+                       .Select(value => (
+                            date: DateTime.Parse(value.Header, CultureInfo.InvariantCulture),
+                            count: double.Parse(value.Value, CultureInfo.InvariantCulture)
+                            )
+                        )
+                       .ToArray()))
+               .GroupBy(c => c.name, c => (c.province, c.values))
+               .Select(c => (
+                    name: c.Key,
+                    count: c.Sum(p => p.values.Sum(x => x.count)),
+                    delta: c.Sum(p => p.values[p.values.Length - 1].count - p.values[p.values.Length - 2].count),
+                    provinces: c.ToArray()
+                    )
+                )
+               .OrderByDescending(c => c.delta).ThenBy(c => c.name)
+               .ToArray();
+
+            var index = 1;
+            foreach (var (name, count, delta, _) in countries_v)
+                Console.WriteLine("{3, 3}{0, 35}:{1}({2})", name, count, delta, index++);
 
 
+            Console.ReadLine();
 
             ////                        3222 2222 2222 1111 1111 1             
             ////                        1098 7654 3210 9876 5432 1098 7654 3210
