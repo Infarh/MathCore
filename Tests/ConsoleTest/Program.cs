41cd6ec3d99ad569d5776606606e2c2c90fb617d
Infarh <shmachilin@yandex.ru>
2020-08-02 13:45:14 +0300

    Подготовка к слияния с dev

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using MathCore.Trees;
+using Microsoft.Data.Analysis;
 using Dirs = System.Collections.Generic.IEnumerable<System.IO.DirectoryInfo>;
 // ReSharper disable ConvertToUsingDeclaration
 
@@ -15,6 +16,23 @@ namespace ConsoleTest
     {
         private static void Main()
         {
+            //                        3222 2222 2222 1111 1111 1             
+            //                        1098 7654 3210 9876 5432 1098 7654 3210
+            var res = (int)Math.Log(0b0000_0001_1000_0000_0000_0001_0000_0000, 2); //24
+
+            var times = new PrimitiveDataFrameColumn<DateTime>("Date");
+            times.Append(DateTime.Now.Subtract(TimeSpan.FromDays(2)));
+            times.Append(DateTime.Now.Subtract(TimeSpan.FromDays(1)));
+            times.Append(DateTime.Now);
+
+            var ints = new PrimitiveDataFrameColumn<int>("ints", 3);
+            var strs = new StringDataFrameColumn("strings", 3);
+
+            var data = new DataFrame(times, ints, strs) {[0, 1] = 10};
+
+
+            Console.ReadLine();
+
             //var dir = new DirectoryInfo(".");
 
             //var dir_tree = dir.AsTreeNode(d => d.EnumerateDirectories(), d => d.Parent);
