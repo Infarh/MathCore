b10254f9d0fd3daa6d21c4be51c3c5b8410e1465
Infarh <shmachilin@yandex.ru>
2020-10-05 12:39:22 +0300

    Реализация шаблона Посредник

--- Tests/WPFTest/ViewModels/MainWindowViewModel.cs
+++ Tests/WPFTest/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,15 @@
-﻿using MathCore.ViewModels;
+﻿using System.Windows.Input;
+using MathCore.Mediator;
+using MathCore.ViewModels;
+using WPFTest.Infrastructure.Commands;
+using WPFTest.Models;
 
 namespace WPFTest.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
+        private readonly IMessenger _Messenger;
+
         #region Title : string - Заголовок окна
 
         /// <summary>Заголовок окна</summary>
@@ -13,5 +19,22 @@ namespace WPFTest.ViewModels
         public string Title { get => _Title; set => Set(ref _Title, value); }
 
         #endregion
+
+        #region Command SendMessageCommand : string - Summary
+
+        /// <summary>Summary</summary>
+        private ICommand _SendMessageCommand;
+
+        /// <summary>Summary</summary>
+        public ICommand SendMessageCommand => _SendMessageCommand
+            ??= new LambdaCommand(OnSendMessageCommandExecuted);
+
+        /// <summary>Проверка возможности выполнения - Summary</summary>
+        private void OnSendMessageCommandExecuted(object p) => _Messenger.Send(this, "To second model",
+            new ServiceMessage(p as string ?? p?.ToString() ?? "Message from Main model"));
+
+        #endregion
+
+        public MainWindowViewModel(IMessenger Messenger) => _Messenger = Messenger;
     }
 }
