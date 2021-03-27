1db6d8c33bfc9b482c5b4785bbba2c6de4309d8f
Infarh <shmachilin@yandex.ru>
2021-03-27 08:28:30 +0300

    Добавлен метод-расширения к Task.Yield для перехода в новый поток для выполнения длительных операций

--- MathCore/Extensions/AsyncAwait/YieldAwaiterExtensions.cs
+++ MathCore/Extensions/AsyncAwait/YieldAwaiterExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+// ReSharper disable UnusedParameter.Global
+// ReSharper disable InvalidXmlDocComment
 
 // ReSharper disable once CheckNamespace
 namespace System.Threading.Tasks
@@ -9,6 +11,9 @@ namespace System.Threading.Tasks
         /// <summary>Продолжить в пуле потоков</summary>
         /// <param name="LockContext">Если истина, то продолжение будет выполнено в том же потоке, если ложь - то в пуле потоков</param>
         public static YieldAwaitableThreadPool ConfigureAwait(this YieldAwaitable _, bool LockContext) => new(LockContext);
+
+        /// <summary>Продолжить в новом потоке</summary>
+        public static YieldAwaitableThread ConfigureAwaitLongRunning(this YieldAwaitable _) => new();
 
         /// <summary>Выполнить продолжение в указанном планировщике</summary>
         /// <param name="Scheduler">Планировщик, в котором требуется выполнить продолжение</param>
