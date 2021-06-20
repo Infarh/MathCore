81ee6e45325ef2fbc5cabc9460d27e0509302611
Infarh <shmachilin@yandex.ru>
2021-06-20 14:13:08 +0300

    Методы-расширения для векторов

--- MathCore/Vectors/Vector2D.Extensions.cs
+++ MathCore/Vectors/Vector2D.Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MathCore.Vectors
 {
@@ -19,11 +21,39 @@ namespace MathCore.Vectors
             return count == 0 ? Vector2D.NaN : new(x_sum / count, y_sum / count);
         }
 
+        public static Vector2D Average<T>(this IEnumerable<T> items, Func<T, Vector2D> VectorSelector)
+        {
+            var x_sum = 0d;
+            var y_sum = 0d;
+            var count = 0;
+            foreach (var (x, y) in items.Select(VectorSelector))
+            {
+                x_sum += x;
+                y_sum += y;
+                count++;
+            }
+
+            return count == 0 ? Vector2D.NaN : new(x_sum / count, y_sum / count);
+        }
+
         public static Vector2D Sum(this IEnumerable<Vector2D> vectors)
         {
             var x_sum = 0d;
             var y_sum = 0d;
             foreach (var (x, y) in vectors)
+            {
+                x_sum += x;
+                y_sum += y;
+            }
+
+            return new(x_sum, y_sum);
+        }
+
+        public static Vector2D Sum<T>(this IEnumerable<T> items, Func<T, Vector2D> VectorSelector)
+        {
+            var x_sum = 0d;
+            var y_sum = 0d;
+            foreach (var (x, y) in items.Select(VectorSelector))
             {
                 x_sum += x;
                 y_sum += y;
--- MathCore/Vectors/Vector3D.Extensions.cs
+++ MathCore/Vectors/Vector3D.Extensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MathCore.Vectors
 {
@@ -21,12 +23,44 @@ namespace MathCore.Vectors
             return count == 0 ? Vector3D.NaN : new(x_sum / count, y_sum / count, z_sum / count);
         }
 
+        public static Vector3D Average<T>(this IEnumerable<T> items, Func<T, Vector3D> VectorSelector)
+        {
+            var x_sum = 0d;
+            var y_sum = 0d;
+            var z_sum = 0d;
+            var count = 0;
+            foreach (var (x, y, z) in items.Select(VectorSelector))
+            {
+                x_sum += x;
+                y_sum += y;
+                z_sum += z;
+                count++;
+            }
+
+            return count == 0 ? Vector3D.NaN : new(x_sum / count, y_sum / count, z_sum / count);
+        }
+
         public static Vector3D Sum(this IEnumerable<Vector3D> vectors)
         {
             var x_sum = 0d;
             var y_sum = 0d;
             var z_sum = 0d;
             foreach (var (x, y, z) in vectors)
+            {
+                x_sum += x;
+                y_sum += y;
+                z_sum += z;
+            }
+
+            return new(x_sum, y_sum, z_sum);
+        }
+
+        public static Vector3D Sum<T>(this IEnumerable<T> items, Func<T, Vector3D> VectorSelector)
+        {
+            var x_sum = 0d;
+            var y_sum = 0d;
+            var z_sum = 0d;
+            foreach (var (x, y, z) in items.Select(VectorSelector))
             {
                 x_sum += x;
                 y_sum += y;
