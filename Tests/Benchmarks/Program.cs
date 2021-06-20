d46a6bb67a517f1282ad373cd787494cff2e4147
Infarh <shmachilin@yandex.ru>
2021-06-20 12:44:13 +0300

    Тесты быстродействия

---
+++ Tests/Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = BenchmarkRunner.Run<Test>();
+        }
+    }
+}
---
+++ Tests/Benchmarks/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class Test
+    {
+        private readonly string[] _Data = Enumerable.Range(1, 1).ToArray(i => i.ToString());
+
+        [Benchmark]
+        public List<int> Lambda()
+        {
+            var result = new List<int>(50000);
+
+            for (var i = 0; i < 50000; i++)
+                result.AddRange(_Data.Select(s => int.Parse(s)));
+            return result;
+        }
+
+        [Benchmark]
+        public List<int> MethodGroup()
+        {
+            var result = new List<int>(50000);
+
+            for (var i = 0; i < 50000; i++)
+                result.AddRange(_Data.Select(int.Parse));
+            return result;
+        }
+    }
+}
