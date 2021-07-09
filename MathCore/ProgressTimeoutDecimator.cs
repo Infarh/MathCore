8ff0e62f771bced62cd30b4124f5c23ed3e4ec4a
Infarh <shmachilin@yandex.ru>
2021-07-09 15:58:44 +0300

    Децимация вызовов в информаторе прогресса

---
+++ MathCore/ProgressTimeoutDecimator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MathCore
+{
+    /// <summary>Информатор прогресса операции, осуществляющий прореживание вызовов по времени</summary>
+    /// <typeparam name="T">Тип данных информатора</typeparam>
+    public class ProgressTimeoutDecimator<T> : IProgress<T>
+    {
+        private readonly IProgress<T> _ProgressSource;
+        private readonly TimeSpan _Timeout;
+
+        /// <summary>Таймаут между вызовами</summary>
+        public TimeSpan Timeout { get => _Timeout; init => _Timeout = value; }
+
+        public ProgressTimeoutDecimator(IProgress<T> ProgressSource) => _ProgressSource = ProgressSource;
+
+        public ProgressTimeoutDecimator(IProgress<T> ProgressSource, TimeSpan Timeout) : this(ProgressSource) => _Timeout = Timeout;
+
+        private readonly Stopwatch _Timer = Stopwatch.StartNew();
+        public void Report(T value)
+        {
+            if (_Timer.Elapsed < _Timeout) return;
+            _Timer.Restart();
+            _ProgressSource.Report(value);
+        }
+    }
+}
