da63bc8144e04e7dedc2ba783d7e020ade966815
Infarh <shmachilin@yandex.ru>
2022-04-13 15:06:06 +0300

    Инструменты упрощения тестирования

---
+++ Tests/MathCore.Tests/Infrastructure/DebugExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+using MathCore;
+
+namespace System.Diagnostics;
+
+internal static class DebugExtensions
+{
+    public static T ToDebug<T>(this T value, [CallerArgumentExpression("value")] string? Prefix = null)
+    {
+        if (Prefix is { Length: > 0 })
+        {
+            FormattableString msg = $"{Prefix} = {value}";
+            Debug.WriteLine(msg.ToString(CultureInfo.InvariantCulture));
+        }
+        else
+        {
+            FormattableString msg = $"{value}";
+            Debug.WriteLine(msg.ToString(CultureInfo.InvariantCulture));
+        }
+        return value;
+    }
+
+    public static void ToDebugEnum<T>(this IEnumerable<T> items, [CallerArgumentExpression("items")] string? Name = null)
+    {
+        if (Name is { Length: > 0 })
+            Debug.WriteLine("{0}[] {1} = {{", typeof(T).Name, Name);
+        var i = 0;
+        var culture = CultureInfo.InvariantCulture;
+        foreach (var item in items)
+        {
+            FormattableString msg = $"/*[{i,2}]*/ {item},";
+            Debug.WriteLine(msg.ToString(culture));
+            i++;
+        }
+        Debug.WriteLine("}");
+    }
+
+    public static void ToDebugEnum(this IEnumerable<Complex> items, [CallerArgumentExpression("items")] string? Name = null)
+    {
+        if (Name is { Length: > 0 })
+            Debug.WriteLine("Complex[] {0} = {{", (object)Name);
+        var i = 0;
+        var culture = CultureInfo.InvariantCulture;
+        foreach (var (re, im) in items)
+        {
+            FormattableString msg = $"/*[{i,2}]*/ ({re:F18}, {im:F18}),";
+            Debug.WriteLine(msg.ToString(culture));
+            i++;
+        }
+        Debug.WriteLine("}");
+    }
+}
