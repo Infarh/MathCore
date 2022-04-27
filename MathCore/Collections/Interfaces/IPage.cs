15be1b3e93a354e396ef52a2624c063a3534d349
Infarh <shmachilin@yandex.ru>
2022-04-27 11:33:16 +0300

    Репозиторий

---
+++ MathCore/Collections/Interfaces/IPage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MathCore.Collections.Interfaces;
+
+/// <summary>Страница элементов</summary>
+/// <typeparam name="T">Тип элементов</typeparam>
+public interface IPage<out T>
+{
+    /// <summary>Индекс страницы</summary>
+    int Index { get; }
+
+    /// <summary>Перечисление элементов страницы</summary>
+    IEnumerable<T> Items { get; }
+
+    /// <summary>Количество элементов на странице</summary>
+    int Count { get; }
+
+    /// <summary>Полное количество элементов в выборке</summary>
+    int TotalCount { get; }
+
+    /// <summary>Размер страницы</summary>
+    int Size { get; }
+
+    /// <summary>Число страниц в выборке</summary>
+    int PagesCount { get; }
+}
