159b36621535337c2df418304ffff936efbf2f39
Infarh <shmachilin@yandex.ru>
2022-05-02 18:32:58 +0300

    0.0.71.1 - Исправление и оптимизация работы методов-расширения для Process - WaitAsync и StartAsync

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,21 +1,33 @@
 
+using System.Diagnostics;
+
 using MathCore.Threading;
 
-var messages = Enumerable.Range(1, 1000).Select(i => $"Message-{i}");
+const string path = @"c:\123\qwe.png";
 
-using (var thread_pool = new InstanceThreadPool(10))
-{
-    foreach (var message in messages)
-        thread_pool.Execute(message, o =>
-        {
-            var msg = (string?)o;
-            Console.WriteLine(">>{0} processing...", msg);
-            Thread.Sleep(5000);
-            Console.WriteLine(">>{0} processing complete", msg);
-        });
+var file = new FileInfo(path);
+
+var execute_process = file.Execute();
+
+var process = await execute_process!
+   .WaitAsync();
 
 
-    Console.ReadLine();
-}
+//var messages = Enumerable.Range(1, 1000).Select(i => $"Message-{i}");
+
+//using (var thread_pool = new InstanceThreadPool(10))
+//{
+//    foreach (var message in messages)
+//        thread_pool.Execute(message, o =>
+//        {
+//            var msg = (string?)o;
+//            Console.WriteLine(">>{0} processing...", msg);
+//            Thread.Sleep(5000);
+//            Console.WriteLine(">>{0} processing complete", msg);
+//        });
+
+
+//    Console.ReadLine();
+//}
 
 Console.ReadLine();
