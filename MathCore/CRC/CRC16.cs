d5340b4030eb92f60034c2542025852f7723127e
Infarh <shmachilin@yandex.ru>
2022-09-02 11:58:08 +0300

    CRC16

--- MathCore/CRC/CRC16.cs
+++ MathCore/CRC/CRC16.cs
@@ -9,13 +9,16 @@ using System.Threading.Tasks;
 
 namespace MathCore.CRC;
 
-public sealed class CRC16
+public class CRC16
 {
     [SuppressMessage("ReSharper", "IdentifierTypo")]
     public enum Mode : ushort
     {
         IBM = 0xA001,
         CCITT = 0x1021,
+        XMODEM = 0x1021,
+        AUG_CCITT = 0x1021,
+        CMS = 0x8005,
         CCITTKermit = 0x8408,
         CCITT16 = 0x8810
     }
@@ -23,21 +26,25 @@ public sealed class CRC16
     private const int __TableLength = 256;
     private readonly ushort[] _Table = new ushort[__TableLength];
 
-    public CRC16(Mode mode = Mode.CCITT)
+    public ushort State { get; set; }
+
+    public bool UpdateState { get; set; }
+
+    public CRC16(Mode mode = Mode.IBM) : this((ushort)mode) { }
+
+    public CRC16(ushort Polynimial)
     {
-        var polynomial = (ushort)mode;
-        for (ushort i = 0; i < __TableLength; ++i)
+        for (var i = 0; i < __TableLength; ++i)
         {
             ushort value = 0;
-            var temp = i;
-            for (byte j = 0; j < 8; ++j)
+            var temp = (ushort)(i << 8);
+            for (var j = 0; j < 8; ++j)
             {
-                if (((value ^ temp) & 0x0001) != 0)
-                    value = (ushort)((value >> 1) ^ polynomial);
+                if (((value ^ temp) & 0x8000) != 0)
+                    value = (ushort)((value << 1) ^ Polynimial);
                 else
-                    value >>= 1;
-
-                temp >>= 1;
+                    value <<= 1;
+                temp <<= 1;
             }
             _Table[i] = value;
         }
@@ -45,21 +52,49 @@ public sealed class CRC16
 
     //public ushort Compute(ReadOnlySpan<byte> bytes)
     //{
-    //    ushort crc = 0;
-    //    for (var i = 0; i < bytes.Length; ++i)
-    //    {
-    //        var index = (byte)(crc ^ bytes[i]);
-    //        crc = (ushort)((crc >> 8) ^ _Table[index]);
-    //    }
+    //    var crc = Initial;
+    //    foreach (var b in bytes)
+    //        crc = (ushort)((crc << 8) ^ _Table[(crc >> 8) ^ b]);
+
     //    return crc;
     //}
 
-    public ushort Compute(params byte[] bytes)
+    public ushort Compute(params byte[] bytes) => ContinueCompute(State, bytes);
+
+    public ushort ContinueCompute(ushort crc, byte[] bytes)
     {
-        ushort crc = 0;
         foreach (var b in bytes)
-            crc = (ushort)((crc >> 8) ^ _Table[(byte)(crc ^ b)]);
+            crc = (ushort)((crc << 8) ^ _Table[(crc >> 8) ^ b]);
+
+        if (UpdateState)
+            State = crc;
+
         return crc;
+    }
+
+    public ushort Compute(IReadOnlyList<byte> bytes) => ContinueCompute(State, bytes);
+
+    public ushort ContinueCompute(ushort crc, IReadOnlyList<byte> bytes)
+    {
+        foreach (var b in bytes)
+            crc = (ushort)((crc << 8) ^ _Table[(crc >> 8) ^ b]);
+
+        if (UpdateState)
+            State = crc;
+
+        return crc;
+    }
+
+    public void Compute(ref ushort crc, byte[] bytes)
+    {
+        foreach (var b in bytes)
+            crc = (ushort)((crc << 8) ^ _Table[(crc >> 8) ^ b]);
+    }
+
+    public void Compute(ref ushort crc, IReadOnlyList<byte> bytes)
+    {
+        foreach (var b in bytes)
+            crc = (ushort)((crc << 8) ^ _Table[(crc >> 8) ^ b]);
     }
 
     public byte[] ComputeChecksumBytes(params byte[] bytes)
---
+++ Tests/MathCore.Tests/CRC/CRC16Tests.cs
@@ -0,0 +1,20 @@
+﻿using MathCore.CRC;
+
+namespace MathCore.Tests.CRC;
+
+[TestClass]
+public class CRC16Tests
+{
+    [TestMethod]
+    public void Poly_1021_initial_0000_data_3FA2132103_crc_718E()
+    {
+        var data = new byte[] { 0x3F, 0xA2, 0x13, 0x21, 0x03 };
+        const ushort expected_crc = 0x718E;
+
+        var crc = new CRC16();
+
+        var actual_crc = crc.Compute(data);
+
+        Assert.That.Value(actual_crc).IsEqual(expected_crc);
+    }
+}
