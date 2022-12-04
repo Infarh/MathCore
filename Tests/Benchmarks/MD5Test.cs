18ea552a610296948857f628818c8d72b14a2c3f
Infarh <shmachilin@yandex.ru>
2022-12-05 00:06:20 +0300

    0.0.83.1 - Добавлен метод-расширения для FileInfo для вычисления MD5 вв аснхронном режиме

---
+++ Tests/Benchmarks/MD5Test.cs
@@ -0,0 +1,59 @@
+﻿using StandardMD5 = System.Security.Cryptography.MD5;
+using CustomMD5 = MathCore.Hash.MD5;
+
+namespace Benchmarks;
+
+[MemoryDiagnoser]
+public class MD5Test
+{
+    private byte[] _Data;
+    private readonly byte[] _Hash = new byte[16];
+    private Stream _DataStream;
+
+    [Params(32, 128, 1024 * 1024 / 2, 1024 * 1024, 1024 * 1024 * 10)]
+    public int DataLength { get; set; }
+
+    [GlobalSetup]
+    public void Initialize()
+    {
+        var data = new byte[DataLength];
+        Random.Shared.NextBytes(data);
+
+        _Data       = data;
+        _DataStream = new MemoryStream(data);
+    }
+
+    [Benchmark]
+    public byte[] Standard()
+    {
+        var hash = StandardMD5.HashData(_DataStream);
+        return hash;
+    }
+
+    [Benchmark]
+    public void StandardDest() => StandardMD5.HashData(_DataStream, _Hash);
+
+    [Benchmark(Baseline = true)]
+    public async Task<byte[]> StandardAsync()
+    {
+        var hash = await StandardMD5.HashDataAsync(_DataStream);
+        return hash;
+    }
+
+    [Benchmark]
+    public async Task StandardDestAsync() => await StandardMD5.HashDataAsync(_DataStream, _Hash);
+
+    [Benchmark]
+    public byte[] CustomStream()
+    {
+        var hash = CustomMD5.Compute(_DataStream);
+        return hash;
+    }
+
+    [Benchmark]
+    public async Task<byte[]> CustomStreamAsync()
+    {
+        var hash = await CustomMD5.ComputeAsync(_DataStream);
+        return hash;
+    }
+}
