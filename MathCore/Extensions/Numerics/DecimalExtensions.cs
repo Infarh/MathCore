6f7b58dce599e185224b1dc3c9f5d769d322a883
Павел <shmachilin@gmail.com>
2023-08-06 01:28:57 +0300

    Методы-расширения для decimal

--- MathCore/Extensions/Numerics/DecimalExtensions.cs
+++ MathCore/Extensions/Numerics/DecimalExtensions.cs
@@ -1,23 +1,46 @@
 ﻿// ReSharper disable once CheckNamespace
+using MathCore.Functions.Differentiable;
+
 namespace System;
 
 /// <summary>Расширения для чисел двойной точности</summary>
 public static class DecimalExtensions
 {
+    public const decimal Pi = 3.14159265358979323846264338327950288419716939937510M;
+    public const decimal Epsilon = 0.0000000000000000001M;
+    private const decimal PIx2 = 6.28318530717958647692528676655900576839433879875021M;
+    public const decimal E = 2.7182818284590452353602874713526624977572470936999595749M;
+    private const decimal PIdiv2 = 1.570796326794896619231321691639751442098584699687552910487M;
+    private const decimal PIdiv4 = 0.785398163397448309615660845819875721049292349843776455243M;
+    private const decimal Einv = 0.3678794411714423215955237701614608674458111310317678M;
+    private const decimal Log10Inv = 0.434294481903251827651128918916605082294397005803666566114M;
+    private const decimal Half = 0.5M;
+
     /// <summary>Вычисление квадратного корня указанной точности последовательными приближениями</summary>
     /// <param name="x">Число, квадратный корень которого требуется вычислить</param>
     /// <param name="epsilon">Требуемая точность</param>
     /// <returns>Квадратный корень числа</returns>
     [DST]
-    public static decimal Sqrt(this decimal x, decimal epsilon = 0.0M)
+    public static decimal Sqrt(this decimal x, decimal epsilon = decimal.Zero)
     {
+        switch (x)
+        {
+            case < 0m: throw new ArgumentOutOfRangeException(nameof(x), x, "Мнимый результат");
+            case 0m: return 0m;
+            case 1m: return 1m;
+            case 4m: return 2m;
+            case 9m: return 3m;
+            case 16m: return 4m;
+            case 25m: return 5m;
+        }
+
         var current = (decimal)Math.Sqrt((double)x);
         decimal previous;
         do
         {
             previous = current;
-            if (previous == 0.0M) return 0;
-            current = (previous + x / previous) / 2;
+            if (previous == decimal.Zero) return decimal.Zero;
+            current = (previous + x / previous)  * Half;
         }
         while (Math.Abs(previous - current) > epsilon);
         return current;
@@ -27,7 +50,7 @@ public static class DecimalExtensions
     /// <param name="x">Проверяемое число</param>
     /// <returns>Истина, если число целое</returns>
     [DST]
-    public static bool IsInt(this decimal x) => decimal.Round(x) - x == 0;
+    public static bool IsInt(this decimal x, decimal epsilon = decimal.Zero) => Math.Abs(decimal.Truncate(x) - x) <= epsilon;
 
     [DST]
     public static decimal Round(this decimal x) => decimal.Round(x);
@@ -40,6 +63,253 @@ public static class DecimalExtensions
     public static decimal Floor(this decimal x) => decimal.Floor(x);
     [DST]
     public static decimal Ceiling(this decimal x) => decimal.Ceiling(x);
+
+    [Copyright("https://github.com/raminrahimzada/CSharp-Helper-Classes/blob/master/Math/DecimalMath/DecimalMath.cs#L207")]
+    public static decimal Exp(this decimal x)
+    {
+        var count = 0;
+
+        if (x > decimal.One)
+        {
+            count = decimal.ToInt32(decimal.Truncate(x));
+            x -= decimal.Truncate(x);
+        }
+
+        if (x < decimal.Zero)
+        {
+            count = decimal.ToInt32(decimal.Truncate(x) - 1);
+            x = decimal.One + (x - decimal.Truncate(x));
+        }
+
+        var iteration = 1;
+        var result = decimal.One;
+        var factorial = decimal.One;
+        decimal cached_result;
+        do
+        {
+            cached_result = result;
+            factorial *= x / iteration++;
+            result += factorial;
+        } while (cached_result != result);
+
+        if (count == 0)
+            return result;
+
+        return result * E.Pow(count);
+    }
+
+    [Copyright("https://github.com/raminrahimzada/CSharp-Helper-Classes/blob/master/Math/DecimalMath/DecimalMath.cs#L207")]
+    public static decimal Log(this decimal x)
+    {
+        if (x <= decimal.Zero) throw new ArgumentException("x must be greater than zero");
+
+        var count = 0;
+        while (x >= decimal.One)
+        {
+            x *= Einv;
+            count++;
+        }
+
+        while (x <= Einv)
+        {
+            x *= E;
+            count--;
+        }
+
+        x--;
+        
+        if (x == decimal.Zero) return count;
+        
+        var result = decimal.Zero;
+        var iteration = 0;
+        var y = decimal.One;
+        var cache_result = result - decimal.One;
+        
+        const int max_iteration = 100;
+        while (cache_result != result && iteration < max_iteration)
+        {
+            iteration++;
+            cache_result = result;
+            y *= -x;
+            result += y / iteration;
+        }
+
+        return count - result;
+    }
+
+    public static decimal Log10(this decimal x) => Log(x) * Log10Inv;
+
+    private static void TruncateToPeriodicInterval(ref decimal x)
+    {
+        while (x >= PIx2)
+        {
+            var divide = Math.Abs(decimal.ToInt32(x / PIx2));
+            x -= divide * PIx2;
+        }
+
+        while (x <= -PIx2)
+        {
+            var divide = Math.Abs(decimal.ToInt32(x / PIx2));
+            x += divide * PIx2;
+        }
+    }
+    public static decimal Sin(this decimal x) => CalculateSinFromCos(x, Cos(x));
+
+    public static decimal Cos(this decimal x)
+    {
+        //truncating to  [-2*PI;2*PI]
+        TruncateToPeriodicInterval(ref x);
+
+        // now x in (-2pi,2pi)
+        switch (x)
+        {
+            case >= Pi and <= PIx2:
+                return -Cos(x - Pi);
+            case >= -PIx2 and <= -Pi:
+                return -Cos(x + Pi);
+        }
+
+        x *= x;
+        //y=1-x/2!+x^2/4!-x^3/6!...
+
+        var xx = -x * Half;
+        var y = decimal.One + xx;
+        var cached_y = y - decimal.One;//init cache  with different value
+
+        const int max_iteration = 100;
+        for (var i = 1; cached_y != y && i < max_iteration; i++)
+        {
+            cached_y = y;
+            decimal factor = i * ((i << 1) + 3) + 1; //2i^2+2i+i+1=2i^2+3i+1
+            factor = -Half / factor;
+            xx *= x * factor;
+            y += xx;
+        }
+
+        return y;
+    }
+
+    private static bool IsSignOfSinePositive(decimal x)
+    {
+        //truncating to  [-2*PI;2*PI]
+        TruncateToPeriodicInterval(ref x);
+
+        //now x in [-2*PI;2*PI]
+        return x switch
+        {
+            >= -PIx2 and <= -Pi => true,
+            >= -Pi and <= decimal.Zero => false,
+            >= decimal.Zero and <= Pi => true,
+            >= Pi and <= PIx2 => false,
+            _ => throw new ArgumentException(nameof(x))
+        };
+
+        //will not be reached
+    }
+
+    private static decimal CalculateSinFromCos(decimal x, decimal cos) => IsSignOfSinePositive(x) 
+        ? +Sqrt(decimal.One - cos * cos) 
+        : -Sqrt(decimal.One - cos * cos);
+
+    public static decimal Tan(this decimal x)
+    {
+        var cos = Cos(x);
+        if (cos is decimal.Zero) throw new ArgumentException(nameof(x));
+
+        //calculate sin using cos
+        var sin = CalculateSinFromCos(x, cos);
+        return sin / cos;
+    }
+
+    public static decimal Sinh(this decimal x)
+    {
+        var exp = Exp(x);
+        var exp_inv = decimal.One / exp;
+        return (exp - exp_inv) * Half;
+    }
+
+    public static decimal Cosh(this decimal x)
+    {
+        var exp = Exp(x);
+        var exp_inv = decimal.One / exp;
+        return (exp + exp_inv) * Half;
+    }
+
+    public static decimal Tanh(this decimal x)
+    {
+        var exp = Exp(x);
+        var exp_inv = decimal.One / exp;
+        return (exp - exp_inv) / (exp + exp_inv);
+    }
+
+    public static decimal Asin(this decimal x)
+    {
+        if (x is > decimal.One or < -decimal.One) throw new ArgumentException("x must be in [-1,1]");
+
+        //known values
+        if (x == decimal.Zero) return decimal.Zero;
+        if (x == decimal.One) return PIdiv2;
+
+        //asin function is odd function
+        if (x < decimal.Zero) return -Asin(-x);
+
+        //my optimize trick here
+
+        // used a math formula to speed up :
+        // asin(x)=0.5*(pi/2-asin(1-2*x*x)) 
+        // if x>=0 is true
+
+        var new_x = decimal.One - 2 * x * x;
+
+        //for calculating new value near to zero than current
+        //because we gain more speed with values near to zero
+        if (x.Abs() > new_x.Abs())
+        {
+            var t = Asin(new_x);
+            return Half * (PIdiv2 - t);
+        }
+
+        var y = decimal.Zero;
+        var result = x;
+        decimal cached_result;
+        var i = 1;
+        y += result;
+        var xx = x * x;
+        do
+        {
+            cached_result = result;
+            result *= xx * (decimal.One - Half / i);
+            y += result / ((i << 1) + 1);
+            i++;
+        } while (cached_result != result);
+
+        return y;
+    }
+
+    public static decimal ATan(this decimal x) => x switch
+    {
+        decimal.Zero => decimal.Zero,
+        decimal.One => PIdiv4,
+        _ => Asin(x / Sqrt(decimal.One + x * x))
+    };
+
+    public static decimal Acos(this decimal x) => x switch
+    {
+        decimal.Zero => PIdiv2,
+        decimal.One => decimal.Zero,
+        < decimal.Zero => Pi - Acos(-x),
+        _ => PIdiv2 - Asin(x)
+    };
+
+    public static decimal Atan2(this (decimal x, decimal y) point) => point switch
+    {
+        (> decimal.Zero,               _) => ATan(point.y / point.x),
+        (< decimal.Zero, >= decimal.Zero) => ATan(point.y / point.x) + Pi,
+        (< decimal.Zero,  < decimal.Zero) => ATan(point.y / point.x) - Pi,
+        (             _,  > decimal.Zero) => +PIdiv2,
+        (             _,  < decimal.Zero) => -PIdiv2,
+        _ => throw new ArgumentException("invalid atan2 arguments")
+    };
 
     /// <summary>Получить обратное число</summary>
     /// <param name="x">Инвертируемое число</param>
