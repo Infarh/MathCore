865b0f90209cb0a2c357b0dd0fc97b5bd5457e64
Infarh <shmachilin@gmail.com>
2023-08-14 06:22:58 +0300

    Разные способы генерации случайных чисел

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,12 +1,12 @@
 using MathCore.Statistic;
 
 const    int seed   = 1395601201;
-const double sigma = 3;
+const double sigma  = 3;
 const double mu     = 5;
-const    int count  = 100000;
+const    int count  = 100000000;
 
 var rnd = new Random(seed);
-var xx = rnd.NextNormal(count, sigma, mu);
+var xx = GetRandom4(rnd, count, sigma, mu);
 
 var interval = xx.GetMinMax();
 var min = interval.Min;
@@ -19,8 +19,8 @@ var histogram_dx = interval_length / segments_count;
 var histogram = new int[segments_count];
 foreach (var x in xx)
 {
-    var i = Math.Min(histogram.Length - 1, (int)Math.Floor((x - min) / dx));
-    histogram[i]++;
+    var i0 = Math.Min(segments_count - 1, (int)Math.Floor((x - min) / dx));
+    histogram[i0]++;
 }
 
 var average = 0d;
@@ -32,28 +32,108 @@ var variance = 0d;
 for (var i = 0; i < segments_count; i++)
     variance += (average - (min + (i + 0.5) * dx)).Pow2() * histogram[i];
 variance /= count;
-//var sko = variance.Sqrt();
+var sko = variance.Sqrt();
 
 var variance_restored = variance * count / (count - 1);
 var sko_restored = variance_restored.Sqrt();
 
-var freedom_degree = segments_count - 1;
+var freedom_degree = segments_count - 3;
 
-var distribution = Distributions.NormalGauss(sigma, mu);
+Console.WriteLine("avg:{0:0.000} var:{1:0.000}({2:0.000}) sko:{3:0.000}({4:0.000})", average, variance, variance_restored, sko, sko_restored);
 
-var criteria = 0d;
+var distribution = Distributions.NormalGauss(sko_restored, mu);
+
+var criteria_p = 0d;
+var criteria_n = 0d;
 for (var i = 0; i < segments_count; i++)
 {
     var x = min + (i + 0.5) * dx;
-    var p0 = histogram_dx * distribution(x);
-    var p = (double)histogram[i] / count;
-    criteria += (p0 - p).Pow2() / p0;
+    var expected_p = histogram_dx * distribution(x);
+    var expected_n = expected_p * count;
 
-    Console.WriteLine("{0,2} [{1,5}] - p:{2,-8:0.00000#} - p0:{3,-10:0.0000000#} - d:{4,-10:0.########} - d0:{5,-10:0.########} - h:{6}", 
-        i, histogram[i], p, p0, (p - p0).Abs(), (p0 - p).Pow2() / p0, criteria);
+    var actual_n = histogram[i];
+    var actual_p = (double)actual_n / count;
+
+    criteria_p += (expected_p - actual_p).Pow2() / expected_p;
+    criteria_n += (expected_n - actual_n).Pow2() / expected_n;
+
+    Console.WriteLine("{0,2} [{1,8}] - p:{2,-8:0.00000#} - [{3,8:0}]p0:{4,-10:0.0000000#} - [{5,7:0}]d:{6,-10:0.########} - [{7,8:0.00}]d0:{8,-10:0.########} - h:{9:0.00}", 
+        i, 
+        histogram[i], actual_p, 
+        expected_n, expected_p, 
+        (expected_n - actual_n).Abs(), (actual_p - expected_p).Abs(),
+        (expected_n - actual_n).Pow2() / expected_n, (expected_p - actual_p).Pow2() / expected_p, 
+        criteria_p * count);
 }
 
-var quantile = SpecialFunctions.Distribution.Student.QuantileHi2(0.99, freedom_degree);
+criteria_p *= count;
 
-Console.WriteLine("chi:{0} - q:{1}", criteria, quantile);
+var quantile = SpecialFunctions.Distribution.Student.QuantileHi2(0.1, freedom_degree);
+
+Console.WriteLine("chi:{0:0.00} - q:{1:0.00}", criteria_p, quantile);
 Console.ReadLine();
+
+//static double[] GetRandom(Random rnd, int Count, double Sgm, double Mu) => rnd.NextNormal(Count, Sgm, Mu);
+
+static double[] GetRandom2(Random rnd, int Count, double Sgm, double Mu)
+{
+    var xx = new double[count];
+
+    for(var i = 0; i < count; i++)
+        xx[i] = GetRandom2(rnd, Sgm, Mu);
+
+    return xx;
+
+    static double GetRandom2(Random rnd, double Sgm, double Mu)
+    {
+        var r2 = 0d;
+        var x = rnd.NextDouble() * 2 - 1;
+        for (var y = rnd.NextDouble() * 2 - 1; r2 is > 1 or 0d; y = x, x = rnd.NextDouble() * 2 - 1)
+            r2 = x * x + y * y;
+
+        return Math.Sqrt(-2 * Math.Log(r2) / r2) * x * Sgm + Mu;
+    }
+}
+
+static double[] GetRandom3(Random rnd, int Count, double Sgm, double Mu)
+{
+    var xx = new double[count];
+
+    for (var i = 0; i < count; i++)
+        xx[i] = GetRandom3(rnd, Sgm, Mu, 5);
+
+    return xx;
+
+    static double GetRandom3(Random rnd, double Sgm, double Mu, int n)
+    {
+        var s = 1d;
+        for (var i = 0; i < n; i++)
+            s *= rnd.NextDouble();
+
+        return (n * s * 2 - 1) * Sgm + Mu;
+    }
+}
+
+static double[] GetRandom4(Random rnd, int Count, double Sgm, double Mu)
+{
+    var xx = EnumRandom4(rnd, Sgm, Mu).Take(count).ToArray();
+
+    return xx;
+
+    static IEnumerable<double> EnumRandom4(Random rnd, double Sgm, double Mu)
+    {
+        while (true)
+        {
+            var u1 = 1 - rnd.NextDouble();
+            var u2 = 1 - rnd.NextDouble();
+
+            var sqrt = Math.Sqrt(-2 * Math.Log(u1));
+
+            var z1 = sqrt * Math.Cos(2 * Math.PI * u2);
+            yield return z1 * Sgm + Mu;
+
+            var z2 = sqrt * Math.Sin(2 * Math.PI * u2);
+            yield return z2 * Sgm + Mu;
+        }
+    }
+}
