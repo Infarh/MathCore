425e9b5bfdf1f6d64873fb6b5723cc15d09eb8f7
Павел <shmachilin@gmail.com>
2023-10-13 12:05:47 +0300

    Тесты

--- Tests/MathCore.Algorithms/Program.cs
+++ Tests/MathCore.Algorithms/Program.cs
@@ -1,34 +1,29 @@
-﻿using MathCore;
+﻿using System.Collections;
+
 using MathCore.Algorithms.Collections;
 using MathCore.Text;
 
-
-
-var bas = StringValueComparer.Base;
-//StringValueComparer.Base = 842003174;
-Console.WriteLine(StringValueComparer.Base);
-var dict = new CustomDictionary<string, int>(new StringValueComparer());
-
-for (var i = 0; i < 1000; i++)
+var comparer = new FixedStringComparer
 {
-    dict.Add($"value-{i}", i);
-}
+    { "111", 3518592 },
+    { "222", 3618594 },
+    { "333", 3718589 },
+    { "444", 3818591 },
+    { "555", 3918594 },
+    { "666", 4018594 },
+    { "777", 4118594 },
+};
 
-for (var i = 0; i < 1000; i++)
-{
-    var value = dict[$"value-{i}"];
-    if (value != i)
-    {
+var dict = new CustomDictionary<string, int>(7, comparer);
 
-    }
-}
+dict.Add("111", 111);
+dict.Add("222", 222);
+dict.Add("333", 333);
+dict.Add("444", 444);
+dict.Add("555", 555);
+dict.Add("666", 666);
+dict.Add("777", 777);
 
-//dict.Add("222", 222);
-//dict.Add("444", 444);
-//dict.Add("777", 777);
-//dict.Add("123", 123);
-//dict.Add("321", 321);
-//dict.Add("111", 111);
 
 //var v222 = dict["222"];
 //var v444 = dict["444"];
@@ -40,3 +35,17 @@ for (var i = 0; i < 1000; i++)
 Console.WriteLine("End.");
 //Console.ReadLine();
 
+class FixedStringComparer(Dictionary<string, int> Codes) : IEqualityComparer<string>, IEnumerable<KeyValuePair<string, int>>
+{
+    public FixedStringComparer() : this(new()) { }
+
+    public bool Equals(string x, string y) => string.Equals(x, y);
+
+    public int GetHashCode(string obj) => Codes.GetValueOrDefault(obj, obj.GetHashCode());
+
+    public void Add(string str, int hash) => Codes[str] = hash;
+
+    public IEnumerator<KeyValuePair<string, int>> GetEnumerator() => Codes.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
