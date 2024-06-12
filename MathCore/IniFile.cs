0069ca8a8967d5f48353894c0d55710e9e7a2d1f
Павел <shmachilin@gmail.com>
2024-06-12 11:35:56 +0300

    Добавлен класс для чтения данных из ini-файлов

---
+++ MathCore/IniFile.cs
@@ -0,0 +1,31 @@
+﻿#nullable enable
+
+using System.Text;
+
+namespace MathCore;
+
+public static class IniFile
+{
+    public record struct Value(string Category, string Name, string Data);
+
+    public static IEnumerable<Value> Read(FileInfo file, Encoding? encoding = null)
+    {
+        var category = string.Empty;
+        foreach (var line in file.GetStringLines(encoding ?? Encoding.UTF8).Where(l => l is { Length: > 2 } and not [';', ..]))
+        {
+            if (line.AsStringPtr().Trim().IsInBracket('[', ']'))
+            {
+                category = line.AsStringPtr(1, -1);
+                continue;
+            }
+
+            var index = line.IndexOf('=');
+            if (index < 0) continue;
+
+            var name = line.AsStringPtr(0, index).Trim().ToString();
+            var value = line.AsStringPtr(index + 1).Trim().ToString();
+
+            yield return new(category, name, value);
+        }
+    }
+}
