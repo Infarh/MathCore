7da5c9b1669561a37caf6a6041089ad9918ce522
Infarh <shmachilin@gmail.com>
2024-06-12 11:33:53 +0300

    Расширения для исключений

---
+++ MathCore/Extensions/ExceptionEx.cs
@@ -0,0 +1,11 @@
+﻿namespace MathCore.Extensions;
+
+public static class ExceptionEx
+{
+    public static TException WithData<TException>(this TException exception, object Key, object Value)
+       where TException : Exception
+    {
+        exception.Data[Key] = Value;
+        return exception;
+    }
+}
