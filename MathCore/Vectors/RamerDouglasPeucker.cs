e7fb86ac9d67d804d4dff1af33ef0a8284717c75
Infarh <shmachilin@gmail.com>
2024-07-06 17:39:01 +0300

    Реализация алгоритма Дугласа-Пекера

---
+++ MathCore/Vectors/RamerDouglasPeucker.cs
@@ -0,0 +1,60 @@
+﻿namespace MathCore.Vectors;
+
+// https://ru.wikipedia.org/wiki/Алгоритм_Рамера_—_Дугласа_—_Пекера
+// https://habr.com/ru/articles/448618/
+public static class RamerDouglasPeucker
+{
+    public static Vector2D[] Smooth(IReadOnlyList<Vector2D> points, double eps)
+    {
+        var eps2 = eps * eps;
+
+        var len = new double[points.Count];
+        (len[0], len[^1]) = (eps, eps);
+
+        var last_point = points.Count - 1;
+        var (start, end) = (0, last_point);
+
+        while (start < end)
+        {
+            var segment = (points[start], points[end]);
+            var max = eps2;
+            var max_index = 0;
+            for (var (i, i1) = (start + 1, end - 1); i <= i1; i++)
+            {
+                var p = points[i];
+                var l = Length2ToLine(p, segment);
+                if (l < max) continue;
+
+                max_index = i;
+                max = l;
+            }
+
+            if (max_index == 0)
+                (start, end) = (end, last_point);
+            else
+            {
+                end = max_index;
+                len[end] = max;
+            }
+        }
+
+        var result = new List<Vector2D>(points.Count / 2);
+        for (var i = 0; i <= last_point; i++)
+            if (len[i] > 0)
+                result.Add(points[i]);
+
+        return [.. result];
+    }
+
+    private static double Length2ToLine(Vector2D p0, (Vector2D p1, Vector2D p2) segment)
+    {
+        var (x0, y0) = p0;
+        var ((x1, y1), (x2, y2)) = segment;
+
+        var dx = x2 - x1;
+        var dy = y2 - y1;
+
+        var d = dy * x0 - dx * y0 + y1 * x2 - y2 * x1;
+        return d * d / (dx * dx + dy * dy);
+    }
+}
---
+++ Tests/MathCore.Tests/Vectors/RamerDouglasPeuckerTests.cs
@@ -0,0 +1,44 @@
+﻿using MathCore.Vectors;
+
+namespace MathCore.Tests.Vectors;
+
+[TestClass]
+public class RamerDouglasPeuckerTests
+{
+    [TestMethod]
+    public void SmoothTest() // https://habr.com/ru/articles/448618/
+    {
+        Vector2D[] points = [(1, 5), (2, 3), (5, 1), (6, 4), (9, 6), (11, 4), (13, 3), (14, 2), (18, 5)];
+
+        //  ^
+        // 7|                         (9,6)                           
+        // 6| (1,5)                     *                       (18,5)
+        // 5|   *            (6,4)                                 *  
+        // 4|    (2,3)         +              +  (13,3)               
+        // 3|      +                       (11,4)   +                 
+        // 2|                                          *              
+        // 1|               *                       (14,2)            
+        // 0|             (5,1)                                       
+        //  +---------------------------------------------------------->
+        //  0   1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19
+
+        Vector2D[] expected_0_5 = [(1,5),   (2,3),   (5,1),   (6,4),   (9,6), /*(11,4),*/ /*(13,3),*/ (14,2), (18,5)];
+        Vector2D[] expected_1_0 = [(1,5), /*(2,3),*/ (5,1),   (6,4),   (9,6), /*(11,4),*/ /*(13,3),*/ (14,2), (18,5)];
+        Vector2D[] expected_1_5 = [(1,5), /*(2,3),*/ (5,1), /*(6,4),*/ (9,6), /*(11,4),*/ /*(13,3),*/ (14,2), (18,5)];
+
+        const double eps_0_5 = 0.5;
+        const double eps_1_0 = 1.0;
+        const double eps_1_5 = 1.5;
+        var smooth_path_0_5 = RamerDouglasPeucker.Smooth(points, eps_0_5);
+        var smooth_path_1_0 = RamerDouglasPeucker.Smooth(points, eps_1_0);
+        var smooth_path_1_5 = RamerDouglasPeucker.Smooth(points, eps_1_5);
+
+        //var smooth_path_0_5_str = smooth_path_0_5.ToArrayFormattedString();
+        //var smooth_path_1_0_str = smooth_path_1_0.ToArrayFormattedString();
+        //var smooth_path_1_5_str = smooth_path_1_5.ToArrayFormattedString();
+
+        smooth_path_0_5.AssertEquals(expected_0_5);
+        smooth_path_1_0.AssertEquals(expected_1_0);
+        smooth_path_1_5.AssertEquals(expected_1_5);
+    }
+}
