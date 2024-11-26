c6d145639f1b1928aaebad991c1c7d7669dbf6e7
Infarh <shmachilin@yandex.ru>
2024-11-26 10:10:18 +0300

    Test SVD

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,69 +1,131 @@
-
-using System.IO.Compression;
-using System.IO.Hashing;
-
-using MathCore.Hash.CRC;
-
-const string data_file_name = "data.file";
-const string zip_file_name = $"{data_file_name}.zip";
-
-using (var file = File.CreateText(data_file_name))
-    for(var i = 0; i < 1000; i++)
-        file.WriteLine("Hello World!!!");
-
-File.Delete(zip_file_name);
-using (var zip = ZipFile.Open(zip_file_name, ZipArchiveMode.Create))
-{
-    var entry = zip.CreateEntry(data_file_name, CompressionLevel.SmallestSize);
-    using var entry_stream = entry.Open();
-    using var file = File.OpenRead(data_file_name);
-    file.CopyTo(entry_stream);
-}
-
-string zip_crc;
-using (var zip = ZipFile.Open(zip_file_name, ZipArchiveMode.Read))
-{
-    var entry = zip.GetEntry(data_file_name);
-    zip_crc = entry.Crc32.ToString("X8");
-}
-
-//var data_file = new FileInfo(data_file_name);
-//var data = File.ReadAllBytes(data_file_name);
-//var ccc = CRC32.Hash(data).ToString("X8");
-//var cc1 = BitConverter.ToUInt32(Crc32.Hash(data)).ToString("X8");
-
-var data1 = "Hello World!"u8.ToArray();
-
-var zz = new Crc32();
-
-var cc3 = Crc32.HashToUInt32(data1).ToString("b32");
-
-//var q = CRC32.Hash(data1, poly: 0x77073096).ToString("b32");
-
-var tabls = CRC32.GetTableReverseBits(0x77073096);
-
-var q = Crc32.HashToUInt32("123456789"u8).ToString("X8");
-
-for (var i = 0u; i < 256; i++)
-{
-    if(i % 8 == 0) Console.WriteLine();
-
-    //var poly = CRC32.Table(i, 0x77073096);
-    var poly = tabls[i];
-    Console.Write($"0x{poly:X8} ");
-}
-
-var ccrc32 = new CRC32(0x77073096)
-{
-    State = 0xFFFFFFFF,
-    XOR = 0xFFFFFFFF,
+double[,] A = {
+    { 2, 1 },
+    { 1, 3 },
+    { 4, 2 },
+    { 3, 4 }
 };
-var cc4 = ccrc32.Compute(data1).ToString("b32");
-//const uint expected_crc = 0x7AC1161F;
 
+double[] b = [5, 6, 10, 12];
 
-//var crc32 = data_file.ComputeCRC32().ToString("X8");
-//var crc322 = data_file.ComputeCRC32().ToString("X8");
+double[] x = Ex.SolveLeastSquares(A, b);
+
+Console.WriteLine("Решение:");
+foreach (var xi in x)
+{
+    Console.WriteLine(xi);
+}
 
 Console.WriteLine("End.");
 return;
+
+static class Ex
+{
+    public static double[] SolveLeastSquares(double[,] A, double[] b)
+    {
+        var m = A.GetLength(0);
+        var n = A.GetLength(1);
+
+        var At = Transpose(A);
+        var AtA = Multiply(At, A);
+        var Atb = Multiply(At, b);
+
+        var x = Solve(AtA, Atb);
+
+        return x;
+    }
+
+    private static double[,] Transpose(double[,] matrix)
+    {
+        var m = matrix.GetLength(0);
+        var n = matrix.GetLength(1);
+        var transposed = new double[n, m];
+
+        for (var i = 0; i < m; i++)
+            for (var j = 0; j < n; j++)
+                transposed[j, i] = matrix[i, j];
+
+        return transposed;
+    }
+
+    private static double[,] Multiply(double[,] A, double[,] B)
+    {
+        var m = A.GetLength(0);
+        var n = A.GetLength(1);
+        var p = B.GetLength(1);
+        var result = new double[m, p];
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < p; j++)
+            {
+                result[i, j] = 0;
+                for (var k = 0; k < n; k++)
+                    result[i, j] += A[i, k] * B[k, j];
+            }
+        }
+
+        return result;
+    }
+
+    private static double[] Multiply(double[,] A, double[] b)
+    {
+        var m = A.GetLength(0);
+        var n = A.GetLength(1);
+        var result = new double[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            result[i] = 0;
+            for (var j = 0; j < m; j++)
+                result[i] += A[j, i] * b[j];
+        }
+
+        return result;
+    }
+
+    private static double[] Solve(double[,] A, double[] b)
+    {
+        var n = A.GetLength(0);
+        var x = new double[n];
+        var L = new double[n, n];
+        var U = new double[n, n];
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j <= i; j++)
+            {
+                double sum = 0;
+                for (var k = 0; k < j; k++)
+                    sum += L[i, k] * U[k, j];
+                L[i, j] = A[i, j] - sum;
+            }
+
+            for (var j = i; j < n; j++)
+            {
+                double sum = 0;
+                for (var k = 0; k < i; k++)
+                    sum += L[i, k] * U[k, j];
+                U[i, j] = (A[i, j] - sum) / L[i, i];
+            }
+        }
+
+        var y = new double[n];
+        for (var i = 0; i < n; i++)
+        {
+            double sum = 0;
+            for (var j = 0; j < i; j++)
+                sum += L[i, j] * y[j];
+            y[i] = (b[i] - sum) / L[i, i];
+        }
+
+        for (var i = n - 1; i >= 0; i--)
+        {
+            double sum = 0;
+            for (var j = i + 1; j < n; j++)
+                sum += U[i, j] * x[j];
+            x[i] = y[i] - sum;
+        }
+
+        return x;
+    }
+}
