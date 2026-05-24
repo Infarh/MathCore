ca24f7a1be9afddeaa06b4bb22a87e0f81bf9b10
Infarh <shmachilin@gmail.com>
2026-05-24 20:54:16 +0300

    Добавлен тест для метода UpdateText в классе ConsoleProgressBar, проверяющий отсутствие исключений и обновление состояния при отключенной отрисовке

--- Tests/MathCore.Tests/ConsoleProgressBarTests.cs
+++ Tests/MathCore.Tests/ConsoleProgressBarTests.cs
@@ -42,6 +42,25 @@ public class ConsoleProgressBarTests
         progress_bar.Dispose();
     }
 
+    [TestMethod]
+    public void UpdateText_DoesNotThrowAndUpdatesState_WhenRenderingDisabled()
+    {
+        using var progress_bar = new ConsoleProgressBar();
+
+        SetPrivateField(progress_bar, "_CanRender", false);
+
+        var update_text = typeof(ConsoleProgressBar)
+           .GetMethod("UpdateText", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        if (update_text is null)
+            Assert.Fail("UpdateText method not found");
+
+        update_text.Invoke(progress_bar, ["state-only-update"]);
+
+        var current_text = GetPrivateField<string>(progress_bar, "_CurrentText");
+        current_text.AssertEquals("state-only-update");
+    }
+
     private static void InvokeTimerHandler(ConsoleProgressBar ProgressBar)
     {
         var timer_handler = typeof(ConsoleProgressBar)
@@ -62,5 +81,20 @@ public class ConsoleProgressBarTests
             Assert.Fail($"Field '{FieldName}' not found");
 
         field.SetValue(ProgressBar, Value);
+    }
+
+    private static T GetPrivateField<T>(ConsoleProgressBar ProgressBar, string FieldName)
+    {
+        var field = typeof(ConsoleProgressBar)
+           .GetField(FieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+
+        if (field is null)
+            Assert.Fail($"Field '{FieldName}' not found");
+
+        var value = field.GetValue(ProgressBar);
+
+        return value is T typed_value
+            ? typed_value
+            : throw new AssertFailedException($"Field '{FieldName}' has unexpected type");
     }
 }
